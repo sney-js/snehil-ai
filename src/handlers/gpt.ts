c6c302c5b9c8c4d0482fbf11d111e5c9181d1cf7
snehilbhushan <snehil.bhushan@akqa.com>
2023-04-22 22:21:36 +0100

    feat(conv): removed conversation object dependency

--- src/handlers/gpt.ts
+++ src/handlers/gpt.ts
@@ -1,10 +1,7 @@
-import { randomUUID } from 'crypto';
 import OpenAI from '../providers/OpenAI';
 import * as cli from '../ui/cli';
 import getConfig from '../configs/config';
 import { moderateIncomingPrompt } from './moderation';
-
-const conversations = {};
 
 const handleMessageGPT = async (
   conversationID: string,
@@ -15,10 +12,6 @@ const handleMessageGPT = async (
   const config = getConfig();
 
   try {
-    // Get last conversation
-    const lastConversationId = conversations[conversationID];
-
-    // Prompt Moderation
     if (config.promptModerationEnabled) {
       try {
         await moderateIncomingPrompt(prompt);
@@ -26,55 +19,29 @@ const handleMessageGPT = async (
         return error.message;
       }
     }
-
-    // Check if we have a conversation with the user
-    let response: string;
-    if (lastConversationId) {
-      // Handle message with previous conversation
-      response = await chatGPT.ask(prompt, lastConversationId, username);
-    } else {
-      // Create new conversation
-      const convId = randomUUID();
-      const conv = chatGPT.addConversation(convId);
-
-      // Set conversation
-      conversations[conversationID] = conv.id;
-
+    const conv = chatGPT.getConversation(conversationID);
+    if (conv.messages.length === 0) {
       cli.print(
         `[GPT] New conversation for ${conversationID} (ID: ${conv.id})`
       );
-
       // Pre prompt
-      /*
-      if (config.prePrompt != null && config.prePrompt.trim() != '') {
-        cli.print(`[GPT] Pre prompt sent!`);
-        let preprompt = `${config.prePrompt}
-        ----
-Summarise what you have learnt and understood via this setup.`;
-        const prePromptResponse = await chatGPT.ask(preprompt, conv.id);
+      if (config.prePrompt) {
+        cli.print(`[GPT] Sending pre-prompt!`);
+        const prePromptResponse = await chatGPT.ask(config.prePrompt, conv.id);
         cli.print('[GPT] Pre prompt response: ' + prePromptResponse);
-        // let summary = `Summarise what you have learnt and understood via this setup.`;
-        // const summaryResp = await chatGPT.ask(summary, conv.id);
-        // cli.print('[GPT] Pre prompt understanding: ' + summaryResp);
       }
-*/
-
-      response = await chatGPT.ask(prompt, conv.id);
     }
 
-    return response;
+    return await chatGPT.ask(prompt, conversationID, username);
   } catch (error: any) {
     console.error('An error occured', error);
-    return (
-      'An error occured, please contact the administrator. (' +
-      error.message +
-      ')'
-    );
+    return `An error occured, please contact the administrator. (${error.message})`;
   }
 };
 
-const handleResetConversation = async (from: string): Promise<string> => {
-  delete conversations[from];
+const handleResetConversation = async (convID: string): Promise<string> => {
+  const chatGPT = await OpenAI.getInstance().getChatGPT();
+  chatGPT.resetConversation(convID);
   return 'Conversation context has been reset!';
 };
 
